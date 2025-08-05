d141f90bec58fb45319b99da38e6083cc3d35d47
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-08-05 15:09:56 +0530

    Update download_sources.sh

--- scripts/download_sources.sh
+++ scripts/download_sources.sh
@@ -135,7 +135,6 @@ download_sources() {
     {
         [ ! -d freetype ] && echo "git clone --depth 1 '$FREETYPE_URL' freetype"
         [ ! -d lcms ] && echo "git clone --depth 1 https://github.com/mm2/Little-CMS lcms"
-        [ ! -d lcms ] && echo "git clone --depth 1 https://github.com/mm2/Little-CMS lcms"
         [ ! -d openjpeg ] && echo "git clone --depth 1 https://github.com/uclouvain/openjpeg openjpeg"
         [ ! -d libvpx ] && echo "git clone --depth 1 https://chromium.googlesource.com/webm/libvpx"
         [ ! -d libxml2 ] && echo "git clone --depth 1 '$LIBXML2_URL' libxml2"
